a1cc14cea99b991632772968fa9fcf521acb49dc
Nathan Watkins <nathancwatkins23@gmail.com>
2023-02-02 14:57:35 -0800

    subject suggestioins main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,89 @@
-"use client";
+'use client'
 
-import React from "react";
-import { useStore } from "../store";
+import React, { useEffect, useState } from 'react'
+import { useStore } from '../store'
+import { topics } from './study/topics'
 
 export default function Home() {
-  const { userId } = useStore();
+    const { userId } = useStore()
+    const [index, setIndex] = React.useState(0)
+    const [subject, setSubject] = React.useState(topics[0])
+    const [text, setText] = useState('')
+    const [increase, setIncrease] = useState(true)
+    const [wait, setWait] = useState(false)
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-gray-800">studymode</h1>
-        <p className="text-lg font-medium text-gray-600">
-          Learn smarter, not harder.
-        </p>
-      </div>
+    useEffect(() => {
+        if (wait) {
+            setTimeout(() => {
+                setWait(false)
+                setIncrease(false)
+            }, 2000)
+        }
+    }, [wait])
 
-      <div className="mt-10">
-        <p className="text-lg font-medium text-gray-600">
-          Create quizzes from your own sources, and test yourself to retain
-          information.
-        </p>
-        <p className="text-lg font-medium text-gray-600">
-          Say goodbye to boring memorization and hello to effective learning.
-        </p>
-      </div>
+    useEffect(() => {
+        let targetText = subject
+        if (!increase && text.length > 0) {
+            const reverseAnimation = setInterval(() => {
+                setText(text.slice(0, -1))
+                if (text.length === 1) {
+                    setIncrease(true)
+                    clearInterval(reverseAnimation)
+                    setIndex((index + 1) % topics.length)
+                    setSubject(topics[index])
+                }
+            }, 50)
+            return () => clearInterval(reverseAnimation)
+        }
+    }, [text, increase])
 
-      <div className="mt-10">
-        <a
-          href={`${userId ? "/quizzes" : "/auth"}`}
-          className="btn bg-blue-500 text-white py-4 px-4 rounded"
-        >
-          Get Started
-        </a>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        let targetText = subject
+        let index = 0
+        if (increase && text !== targetText) {
+            const fowardAnimation = setInterval(() => {
+                setText((text) => (text += targetText[index - 1]))
+                index++
+                if (index === targetText.length) {
+                    clearInterval(fowardAnimation)
+                    setWait(true)
+                }
+            }, 50)
+            return () => clearInterval(fowardAnimation)
+        }
+    }, [increase])
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div>
+                <p className="text-xl font-sm">I want to know...</p>
+            </div>
+            <div className="text-center">
+                <h1 className={` text-5xl font-bold text-gray-800`}>{text}</h1>
+                <p className="text-lg font-medium text-gray-600">
+                    Learn smarter, not harder.
+                </p>
+            </div>
+
+            <div className="mt-10">
+                <p className="text-lg font-medium text-gray-600">
+                    Create quizzes from your own sources, and test yourself to
+                    retain information.
+                </p>
+                <p className="text-lg font-medium text-gray-600">
+                    Say goodbye to boring memorization and hello to effective
+                    learning.
+                </p>
+            </div>
+
+            <div className="mt-10">
+                <a
+                    href={`${userId ? '/quizzes' : '/auth'}`}
+                    className="btn bg-blue-500 text-white py-4 px-4 rounded"
+                >
+                    Get Started
+                </a>
+            </div>
+        </div>
+    )
 }
