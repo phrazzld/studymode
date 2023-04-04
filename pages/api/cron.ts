a2e54b2833b258ab8c7228b1a20feeb6a71299ac
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-04-04 09:02:00 -0500

    Type cron params

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,4 +1,9 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  _req: NextApiRequest,
+  _res: NextApiResponse
+) {
   const query = new URLSearchParams({
     query: "keepalive",
     userId: "keepalive",
